67de4ca3af3ec19235f24c80287a18d680c1fd50
camila badillo <camihyohana@MacBook-Air-de-camila.local>
2018-09-30 23:25:07 -0300

    primero solo base

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php get_header() ?>
+
+<h1>Body de la página de inicio</h1>
+
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+kjsadjhsahjd <i class="fab fa-500px"></i>
+</div>
+<div class="col-md-4">
+kjsadjhsahjd
+</div>
+</div>
+
+</div>
+<?php get_sidebar() ?>
+
+<?php get_footer() ?>
