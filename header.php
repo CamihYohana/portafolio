690ae2c16d078ab8e3ee8ad626cb3d0bf487cfca
camila badillo <camihyohana@MacBook-Air-de-camila.local>
2018-10-04 00:48:31 -0300

    index header

--- header.php
+++ header.php
@@ -3,12 +3,31 @@
 <head>
 	<meta charset="<?php bloginfo('charset') ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php get_template_part('_includes/iOS', 'icons') ?>
 	<?php wp_head() ?>
-</head>
-<body>
+	<title>Portafolio Camila Badillo</title>
+	<meta name="description" content="Camila's portfolio | Web Design, Design & Photgraphy" />
+	<meta name="keywords" content="design, designer, photgraphy, analogue, 35mm" />
+	<meta name="author" content="CamihYohana" />
+	<link rel="stylesheet" href="assets/css/style.css">
+	<!-- <link rel="stylesheet" href="assets/css/icomoon.css"> -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css" integrity="sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW"
+	 crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/regular.css" integrity="sha384-ZlNfXjxAqKFWCwMwQFGhmMh3i89dWDnaFU2/VZg9CvsMGA7hXHQsPIqS+JIAmgEq"
+	 crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/brands.css" integrity="sha384-rf1bqOAj3+pw6NqYrtaE1/4Se2NBwkIfeYbsFdtiR6TQz0acWiwJbv1IM/Nt/ite"
+	 crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css" integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7"
+	 crossorigin="anonymous">
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
 
+</head>
+<body></body>
+</html>
 <?php if ( has_nav_menu( 'header-menu' ) ) { ?>
 	<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'header_menu' ) ); ?>
 <?php } ?>
--- index.php
+++ index.php
@@ -1,18 +1,237 @@
 <?php get_header() ?>
 
-<h1>Body de la página de inicio</h1>
+<body>
+	<!-- <div class="loader"></div> -->
+	<div id="page_">
+		<nav class="nav" role="navigation">
+			<div class="container">
+				<div class="top-logo">
+					<div id="logo"><a href="index.html">.</a></div>
+				</div>
+				<div class="top-menu menu-1 text-center">
+					<ul>
+						<li><a href="work.html">Lo que hago</a></li>
+						<li><a href="about.html">Acerca de mi</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="contact.html">Contacto</a></li>
+					</ul>
+				</div>
+				<div class="top-social menu-1 text-right">
+					<ul class="social">
+						<li><a href="#"><i class="icon-twitter"></i></a></li>
+						<li><a href="#"><i class="icon-instagram"></i></a></li>
+						<li><a href="#"><i class="icon-github"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 
-<div class="container">
-<div class="row">
-<div class="col-md-4">
-kjsadjhsahjd <i class="fab fa-500px"></i>
-</div>
-<div class="col-md-4">
-kjsadjhsahjd
-</div>
-</div>
+		<div id="work">
+			<div class="container">
+				<div class="row top-line animate-box">
+					<div class="col-md-7 col-md-push-5 text-left intro">
+						<h2>TExto de ejemplo titulo <span class="highlight">Made with <i class="icon-heart2"></i>
+								by <a href="http://freehtml5.co">CamihYohana</a> </span></h2>
+					</div>
 
-</div>
-<?php get_sidebar() ?>
+				</div>
+				<div class="row">
+					<div class="col-md-4 text-center animate-box">
+						<a class="work" href="portfolio_detail.html">
+							<div class="work-grid" style="background-image:url(assets/images/project-1.jpg);">
+								<div class="inner">
+									<div class="desc">
+										<h3>Folding Light</h3>
+										<span class="cat">Branding</span>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<a class="work" href="portfolio_detail.html">
+							<div class="work-grid" style="background-image:url(assets/images/project-2.jpg);">
+								<div class="inner">
+									<div class="desc">
+										<h3>Power Led Light</h3>
+										<span class="cat">Illustration</span>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<a class="work" href="portfolio_detail.html">
+							<div class="work-grid" style="background-image:url(assets/images/project-3.jpg);">
+								<div class="inner">
+									<div class="desc">
+										<h3>Elephant Origami</h3>
+										<span class="cat">Logo</span>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<a class="work" href="portfolio_detail.html">
+							<div class="work-grid" style="background-image:url(assets/images/project-4.jpg);">
+								<div class="inner">
+									<div class="desc">
+										<h3>Electric Kick Scooter</h3>
+										<span class="cat">Web Design</span>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<a class="work" href="portfolio_detail.html">
+							<div class="work-grid" style="background-image:url(assets/images/project-5.jpg);">
+								<div class="inner">
+									<div class="desc">
+										<h3>Shark Diver</h3>
+										<span class="cat">Branding</span>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<a class="work" href="portfolio_detail.html">
+							<div class="work-grid" style="background-image:url(assets/images/project-6.jpg);">
+								<div class="inner">
+									<div class="desc">
+										<h3>Objekte Unserer Tage Berlin</h3>
+										<span class="cat">Logo</span>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<a class="work" href="portfolio_detail.html">
+							<div class="work-grid" style="background-image:url(assets/images/project-7.jpg);">
+								<div class="inner">
+									<div class="desc">
+										<h3>Aloha Shirt</h3>
+										<span class="cat">Web Design</span>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<a class="work" href="portfolio_detail.html">
+							<div class="work-grid" style="background-image:url(assets/images/project-8.jpg);">
+								<div class="inner">
+									<div class="desc">
+										<h3>Olafur Elliasson Waterfall Versailles</h3>
+										<span class="cat">Branding</span>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<a class="work" href="portfolio_detail.html">
+							<div class="work-grid" style="background-image:url(assets/images/project-9.jpg);">
+								<div class="inner">
+									<div class="desc">
+										<h3>Hand Care</h3>
+										<span class="cat">Logo</span>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="author" class="bg-section">
+			<div class="container">
+				<div class="row animate-box">
+					<div class="col-md-8 col-md-offset-2 text-center heading">
+						<h2>A little about me</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="author">
+							<div class="author-inner animate-box" style="background-image: url(assets/images/person3.jpg);">
+							</div>
+							<div class="desc animate-box">
+								<span>Web Designer &amp; Developer</span>
+								<h3>Mike Airways</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+									blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
+									ocean.</p>
+								<p><a href="about.html" class="btn btn-primary btn-outline">Learn More</a></p>
+								<ul class="social-icons">
+									<li><a href="#"><i class="icon-facebook"></i></a></li>
+									<li><a href="#"><i class="icon-twitter"></i></a></li>
+									<li><a href="#"><i class="icon-dribbble"></i></a></li>
+									<li><a href="#"><i class="icon-github"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="started">
+			<div class="container">
+				<div class="row animate-box">
+					<div class="col-md-8 col-md-offset-2 text-center heading">
+						<h2>Get Started</h2>
+						<p>Texro de ejemplo lorem ipsum</p>
+						<p><a href="#" class="btn btn-primary">Let's work together</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<footer id="footer" role="contentinfo">
+			<div class="container">
+				<div class="row copyright">
+					<div class="col-md-12 text-center">
+						<p>
+							<small class="block">TExto de ejemplo</small>
+							<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">Ejemplo</a> imagenes <a href="http://unsplash.co/"
+								 target="_blank">Unsplash</a> &amp; <a href="http://blog.gessato.com/" target="_blank">Gessato</a></small>
+						</p>
+
+						<ul class="social-icons">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+
+					</div>
+				</div>
+
+			</div>
+		</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="https://unpkg.com/jquery@3.3.1/dist/jquery.min.js"></script>
+	<!-- <script src="assets/js/jquery.min.js"></script> -->
+	<!-- jQuery Easing -->
+	<!-- <script src="js/jquery.easing.1.3.js"></script> -->
+	<!-- Bootstrap -->
+	<!-- <script src="js/bootstrap.min.js"></script> -->
+	<!-- Waypoints -->
+	<!-- <script src="js/jquery.waypoints.min.js"></script> -->
+	<!-- Main -->
+
+
+</body>
 
 <?php get_footer() ?>
