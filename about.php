48ed91234f7bae72e289ca6696403e3a2e31d5d2
camila badillo <camihyohana@MacBook-Air-de-camila.local>
2018-10-09 23:01:25 -0300

    agregado de blog

--- about.php
+++ about.php
@@ -26,66 +26,144 @@
 			</div>
 		</nav>
 
-	<div id="author">
-		<div class="container">
-			<div class="row top-line animate-box">
-				<div class="col-md-6 col-md-offset-3 col-md-push-2 text-left heading">
-					<h2>Sobre mí</h2>
-					<h3>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h3>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind
-						texts. Separated they live in Bookmarksgrove</p>
+	<div id="page">
+		<nav class="nav" role="navigation">
+			<div class="container">
+				<div class="top-logo">
+					<div id="logo"><a href="index.html">CamihYohana</a></div>
+				</div>
+				<div class="top-menu menu-1 text-center">
+					<ul>
+						<li><a href="work.html">Lo que hago</a></li>
+						<li><a href="about.html">Acerca de mi</a></li>
+						<li><a href="blog.html">Blog</a></li>
+						<li><a href="contact.html">Contacto</a></li>
+					</ul>
+				</div>
+				<div class="top-social menu-1 text-right">
+					<ul class="social">
+						<li><a href="#"><i class="icon-twitter"></i></a></li>
+						<li><a href="#"><i class="icon-instagram"></i></a></li>
+						<li><a href="#"><i class="icon-github"></i></a></li>
+					</ul>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2">
-					<div class="author">
-						<div class="author-inner animate-box" style="background-image: url(<?php echo get_template_directory_uri();?>images/person3.jpg);">
-						</div>
-						<div class="desc animate-box">
-						<span>Diseñadora Gráfica &amp; Diseñadora UI</span>
+		</nav>
+
+		<div id="author">
+			<div class="container">
+				<div class="row top-line animate-box">
+					<div class="col-md-6 col-md-offset-3 col-md-push-2 text-left heading">
+						<h2>Sobre Mí</h2>
+						<h3>Camila Yohana Badillo Carroza</h3>
+						<p>Diseñadora Gráfica &amp; Diseñadora UI | Fotógrafa aficionada</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="author">
+							<div class="author-inner animate-box" style="background-image: url(images/person3.jpg);">
+							</div>
+							<div class="desc animate-box">
+								<span>Diseñadora Gráfica &amp; Diseñadora UI</span>
 								<h3>Camila Yohana Badillo Carroza</h3>
-							
-							<p>Puedes contactarme en</p>
-							<ul class="social-icons">
+
+								<p>Puedes contactarme en</p>
+								<ul class="social-icons">
 									<li><a href="mailto:camilayohanabc@gmail.com"><i class="fas fa-envelope"></i></a></li>
 									<li><a href="http://www.twitter.com/CamihYohana"><i class="fab fa-twitter"></i></a></li>
 									<li><a href="http://www.behance.net/CamihYohana"><i class="fab fa-behance"></i></a></li>
 								</ul>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<h3 class="title animate-box">Lo que hago</h3>
+							</div>
+							<div class="col-md-6 col-sm-6">
+								<div class="feature-center animate-box" data-animate-effect="fadeIn">
+									<span class="icon">
+										<i class="icon-brush"></i>
+									</span>
+									<h3>Diseño Web - UI</h3>
+									<p>Trabajo para entregar los diseños de sus sitios de la mejor manera que los usuarios puedan navegar en su
+										sitio de forma eficiente.</p>
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-6">
+								<div class="feature-center animate-box" data-animate-effect="fadeIn">
+									<span class="icon">
+										<i class="icon-cog"></i>
+									</span>
+									<h3>Diseño Gráfico</h3>
+									<p>Desde tu logo hasta envases o merchandising</p>
+								</div>
+							</div>
+
+							<div class="col-md-6 col-sm-6">
+								<div class="feature-center animate-box" data-animate-effect="fadeIn">
+									<span class="icon">
+										<i class="icon-briefcase"></i>
+									</span>
+									<h3>Fotografía</h3>
+									<p>La fotografía análoga es gran parte de mi vida, ojalá que podamos hacer que de la tuya también.</p>
+								</div>
+							</div>
+
 						</div>
 					</div>
+				</div>
+				<div class="col-md-12">
+					<h3 class="title animate-box">My Skills</h3>
 					<div class="row">
-						<div class="col-md-12">
-							<h3 class="title animate-box">Lo que hago</h3>
-						</div>
-						<div class="col-md-6 col-sm-6">
-							<div class="feature-center animate-box" data-animate-effect="fadeIn">
-								<span class="icon">
-									<i class="icon-brush"></i>
-								</span>
-								<h3>Diseño Web - UI</h3>
-								<p>Trabajo para entregar los diseños de sus sitios de la mejor manera que los usuarios puedan navegar en su sitio de forma eficiente.</p>
+						<div class="col-md-6 animate-box skills">
+							<h3>Diseño WEB - UI</h3>
+							<div class="progress">
+								<div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%">
+									100%
+								</div>
 							</div>
 						</div>
-						<div class="col-md-6 col-sm-6">
-							<div class="feature-center animate-box" data-animate-effect="fadeIn">
-								<span class="icon">
-									<i class="icon-cog"></i>
-								</span>
-								<h3>Diseño Gráfico</h3>
-								<p>Desde tu logo hasta envases o merchandising</p>
+						<div class="col-md-6 animate-box skills">
+							<h3>HTML5/CSS3</h3>
+							<div class="progress">
+								<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:90%">
+									90%
+								</div>
 							</div>
 						</div>
-
-						<div class="col-md-6 col-sm-6">
-							<div class="feature-center animate-box" data-animate-effect="fadeIn">
-								<span class="icon">
-									<i class="icon-briefcase"></i>
-								</span>
-								<h3>Fotografía</h3>
-								<p>La fotografía análoga es gran parte de mi vida, ojalá que podamos hacer que de la tuya también.</p>
+						<div class="col-md-6 animate-box skills">
+							<h3>Fotografía análoga</h3>
+							<div class="progress">
+								<div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width:85%">
+									85%
+								</div>
 							</div>
 						</div>
-						
+						<div class="col-md-6 animate-box skills">
+							<h3>Serigrafía</h3>
+							<div class="progress">
+								<div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width:95%">
+									95%
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 animate-box skills">
+							<h3>Ilustración Abstracta</h3>
+							<div class="progress">
+								<div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width:85%">
+									85%
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 animate-box skills">
+							<h3>Bicicleta</h3>
+							<div class="progress">
+								<div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%">
+									100%
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
---
+++ blog.php
@@ -0,0 +1,87 @@
+
+<body>
+
+	<div id="page">
+		<nav class="nav" role="navigation">
+			<div class="container">
+				<div class="top-logo">
+					<div id="logo"><a href="index.html">CamihYohana</a></div>
+				</div>
+				<div class="top-menu menu-1 text-center">
+					<ul>
+						<li><a href="work.html">Lo que hago</a></li>
+						<li><a href="about.html">Acerca de mi</a></li>
+						<li><a href="blog.html">Blog</a></li>
+						<li><a href="contact.html">Contacto</a></li>
+					</ul>
+				</div>
+				<div class="top-social menu-1 text-right">
+					<ul class="social">
+						<li><a href="#"><i class="icon-twitter"></i></a></li>
+						<li><a href="#"><i class="icon-instagram"></i></a></li>
+						<li><a href="#"><i class="icon-github"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+		<div id="author">
+			<div class="container">
+				<div class="row top-line animate-box">
+					<div class="col-md-6 col-md-offset-3 col-md-push-2 text-left heading">
+						<h2>Arroz Integral</h2>
+						<p>Se debía crear un packaging para un arroz integral</p>
+
+						<div class="role">
+							<h3>Rol</h3>
+							<ul>
+								<li>Investigación</li>
+								<li>Diseño</li>
+								<li>Impresión</li>
+							</ul>
+						</div>
+
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<p class="animate-box"><img src="images/bonita.jpg" class="img-responsive" alt=""></p>
+						<p class="animate-box"><img src="images/bonita-pack.jpg" class="img-responsive" alt=""></p>
+
+						<div class="testimony animate-box">
+							<blockquote>
+								<p>&ldquo;Unca cita debe ir aquí&rdquo;</p>
+								<p class="person"><cite>&mdash; CamilaYohana</cite> Diseñadora</p>
+							</blockquote>
+						</div>
+						<p class="animate-box"><img src="<?php echo get_template_directory_uri();?>bonita-tipo.jpg" class="img-responsive" alt=""></p>
+						<p class="animate-box"><img src="<?php echo get_template_directory_uri();?>bonita-tarro.jpg" class="img-responsive" alt=""></p>
+
+
+						<div class="row row-pt-md portfolio-navigation">
+							<div class="col-xs-4 text-left">
+								<a href="#" class="btn btn-primary btn-outline"><i class="icon-chevron-left"></i> anterior </a>
+							</div>
+							<div class="col-xs-4 text-center">
+								<a href="work.html" class="btn btn-primary btn-outline"><i class="icon-grid2"></i> Ver todos</a>
+							</div>
+							<div class="col-xs-4 text-right">
+								<a href="#" class="btn btn-primary btn-outline">Siguiente <i class="icon-chevron-right"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="started">
+			<div class="container">
+				<div class="row animate-box">
+					<div class="col-md-8 col-md-offset-2 text-center heading">
+						<h2>Comencemos</h2>
+						<p>¿Te interesa comenzar algún trabajo conmigo?</p>
+						<p><a href="mailto:camilayohanabc@gmail.com" class="btn btn-primary">Trabaja conmigo</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
